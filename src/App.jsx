7016105fbdf1c5ef294f97d3e98a6757785c8e9a
Gouri Agarwal <gouriagarwal15@gmail.com>
2024-02-21 22:41:03 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,21 @@ const imageData = ()=>{
 
 function App() {
   // code here
+  let imgdata = imageData();
+  console.log(imgdata);
+  
+  return (
+    <div className="App">
+      <h1>Kalvium Gallery</h1>
+
+      <div className="image">
+        <img src={imgdata[0].img} alt="ele" />
+        <img src={imgdata[1].img} alt="ele" />
+        <img src={imgdata[2].img} alt="ele" />
+        <img src={imgdata[3].img} alt="ele" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
